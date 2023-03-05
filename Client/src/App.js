52de8dbcaf1bb3a62632bff7255b41fc18384ebd
Nicolas Towery <nicolatowery@gmail.com>
2023-03-05 16:19:55 -0600

    socket io changes
    This was a lot of trial and error on my part. Trying to figure out how to transfer an entire javascript object over socket io. So many files because I was debugging trying to get socket io to work. uninstalled and reinstalled packages many times

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,37 +2,38 @@ import './App.css';
 import io from 'socket.io-client';
 import { useEffect, useState } from 'react';
 
-//connection to server
-const socket = io.connect("http://localhost:3000");
+//connects to server
+const socket = io.connect("http://localhost:3001");
+
 
 function App() {
+  //variables used to send and receive games
+  const [game, setGame] = useState("");
+  const [gameReceived, setGameReceived] = useState("");
 
-//variabls (states) used to send and receive game board
-const [game, setGame] = useState("");
-//might not need this state
-const [gameReceived, setGameReceived] = useState("");
+  //when called, send the game variable to the backend
+  const sendGame = () => {
+    socket.emit("send_game", { game })
+  };
 
-//when called, send the game object to the backend
-const sendGame = () => {
-  socket.emit("game_push", { game })
-}
+  //event listener for receieve_game
+  //once event happens, update gameReceieved variable
+  useEffect(() => {
+    socket.on("receive_game", (data) => {
+      console.log(Object.values(data))
+      setGameReceived(Object.values(data))
+    })
+  }, [socket])
 
-//event listener for game_pull
-//once event happens, update the game object
-useEffect(() => {
-  socket.on("game_pull", (data) => {
-    setGame(data.message);
-  })
-}, [socket])
 
   return (
     <div className="App">
-      <input placeholder='Change Game State . . .' onChange={(event) =>{
+      <input placeholder='game...' onChange={(event) => {
         setGame(event.target.value);
-      }}/>
-      <button onClick={sendGame}>Send Game State</button> 
-      <h1>Game State:</h1>
-      { game }     
+        }}/>
+      <button onClick={sendGame}>Send Game</button>
+      <h1>Game</h1>
+      { gameReceived}
     </div>
   );
 }
--- Controller/test_player_action.js
+++ Controller/test_player_action.js
@@ -1,10 +1,14 @@
-const path = require('path');
 const express = require('express');
 const app = express();
 const http = require('http');
+const { Server } = require('socket.io');
+const cors = require("cors")
+
+//cors prevents connection errors
+app.use(cors());
+
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const port = 3000;
+
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:3000",
@@ -12,20 +16,31 @@ const io = new Server(server, {
     }
 });
 
+//test game object
+gameObject = {
+    cards: 12,
+    contested_zone: 4,
+    myTurn: false,
+    myHealth: 0,
+    opponentHealth: 12, 
+    opponentName: 'BAD GUY' 
+  }
 
 //event listener for a client to make a connection
 //A socket represents an individual client
-io.on('connection', (socket) => {
-    console.log(`User Connected: ${socket.id}`);
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`)
 
-    socket.on("push_game", (data) => {
-        console.log(socket.id + data);
+    socket.on("send_game", (data) => {
+        console.log(data);
+        console.log(data.message);
 
-        //broadcast will emit to all other vlients except the on that sent the data
-        socket.broadcast.emit("pull_game");
-    });
-});
+        //broadcast will emit to all other clients except the one that sent the data
+        socket.broadcast.emit("receive_game", gameObject)
+    })
+})
 
-server.listen(port, () => {
-    console.log(`SERVER LISTENING ON PORT ${port}`);
-});
+server.listen(3001, () =>{
+    console.log("SERVER IS RUNNING");
+})
+
